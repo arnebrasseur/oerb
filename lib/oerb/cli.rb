604045d7a9ebb2926a6f676dc634e23ad81f106c
Arne Brasseur <arne@arnebrasseur.net>
2013-04-28 21:52:39 +0200

    Provide a warning message when no input file is specified.

--- lib/oerb/cli.rb
+++ lib/oerb/cli.rb
@@ -11,7 +11,7 @@ module Oerb
 
     def parse_options( args )
       options = @options = {port: 8069}
-      OptionParser.new do |opt|
+      optparse = OptionParser.new do |opt|
         opt.banner << ' [input file]'
         opt.on('-u', '--username USERNAME', 'OpenERP username')            {|u| options[:username] = u }
         opt.on('-p', '--password PASSWORD', 'OpenERP password')            {|p| options[:password] = p }
@@ -19,8 +19,15 @@ module Oerb
         opt.on('-o', '--host HOST', 'OpenERP host')                        {|o| options[:host]     = o }
         opt.on('-P', '--port PORT', 'OpenERP port')                        {|h| options[:port]     = p }
         opt.on('-h', '--help', 'Print this help message')                  { puts opt; exit }
-      end.parse!(args)
+      end
+      optparse.parse!(args)
       @input_file = args.pop
+      unless @input_file
+        puts optparse
+        puts
+        STDERR.puts "WARNING : No input file specified!"
+        exit
+      end
     end
 
     def url
